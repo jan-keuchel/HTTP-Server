8f17528e3cae518c8af64f9449d25ae3f4fa4661
Jan Keuchel <jan@keuchel.de>
2025-11-24 12:07:39 +0100

    Add comments and restructure use of res pointer on getaddrinfo()

--- server.c
+++ server.c
@@ -12,54 +12,67 @@
 int main(void) {
     printf("--- Server ---\n");
 
+    int             s;
+    int             sfd;
     struct addrinfo hints;
-    struct addrinfo *res;
+    struct addrinfo *res, *rp;
 
+    // Set everything to 0
     memset(&hints, 0, sizeof(hints));
 
     hints.ai_family     = AF_UNSPEC;    // IPv4 or IPv6
     hints.ai_socktype   = SOCK_STREAM;  // TCP
-    hints.ai_flags      = AI_PASSIVE;   // wildcard IP address
+    hints.ai_flags      = AI_PASSIVE;   // We want to bind the address
     hints.ai_protocol   = 0;            // any protocol
-    int s = getaddrinfo(NULL, "2390", &hints, &res);
+
+    // Creating the addrinfo structure
+    s = getaddrinfo(NULL, "2390", &hints, &res); // Localhost on port 2390
     if (s != 0) {
         fprintf(stderr, "gai error: %s\n", gai_strerror(errno));
         exit(1);
     }
 
+    // Additional check
     if (res == NULL) {
         fprintf(stderr, "resulting addrinfo is NULL.\n");
         exit(1);
     }
 
-    // socket(domain, type, protocol)
-    // Create a socket (file descriptor)
-    // domain   -> AF_INET for IPv4
-    // type     -> SOCK_STREAM for TCP or SOCK_DGRAM for UDP
-    // protocol -> 0
-    // Returns fd on success or -1 on failure and sets errno
-    int sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sfd == -1) {
-        fprintf(stderr, "sfd error: %s\n", strerror(errno));
-        exit(1);
+    // Find the socket that can be bound
+    for (rp = res; res != NULL; res = res->ai_next) {
+        // Get a file descriptor through clients can be accepted
+        sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        // bind the socket file descriptor to an address
+        if (bind(sfd, res->ai_addr, res->ai_addrlen) == 0)
+            break;
+
+        close(sfd);
     }
 
-    // bind the socket file descriptor to an address
-    if (bind(sfd, res->ai_addr, res->ai_addrlen) != 0) {
-        fprintf(stderr, "bind error: %s\n", strerror(errno));
-        exit(1);
-    }
-
+    // Free heap-allocated data
     freeaddrinfo(res);
 
+    // Check if none of the addrinfo's worked
+    if (rp == NULL) {
+        fprintf(stderr, "Could not bind.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Mark socket sfd as passive: a socket that is used to 
+    // accept incoming clonnection requests
     if (listen(sfd, 10) == -1) {
         fprintf(stderr, "listen error: %s\n", strerror(errno));
         exit(1);
     }
 
-
     struct sockaddr_storage c_addr;
     socklen_t               c_addr_len = sizeof(c_addr);
+
+    // Accept in incoming connection request
+    // This yields a new file descriptor for the specific client
     int cfd = accept(sfd, (struct sockaddr *) &c_addr, &c_addr_len);
     if (cfd == -1) {
         fprintf(stderr, "accept error: %s\n", strerror(errno));
@@ -67,6 +80,7 @@ int main(void) {
     }
 
     char buf[500] = {0};
+    // Read data
     if (recv(cfd, buf, 500, 0) == -1) {
         fprintf(stderr, "recv error: %s\n", strerror(errno));
         exit(1);
@@ -77,5 +91,5 @@ int main(void) {
     close(cfd);
     close(sfd);
 
-
+    exit(EXIT_SUCCESS);
 }
